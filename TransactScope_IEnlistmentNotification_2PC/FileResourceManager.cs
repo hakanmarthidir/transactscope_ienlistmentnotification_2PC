12d74ff0ecdcb8829d75ad4e66092ed90e979b51
hakanmarthidir <hakan_hidir@hotmail.com>
2022-07-12 00:54:39 +0200

    init project

---
+++ TransactScope_IEnlistmentNotification_2PC/FileResourceManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Text;
+using System.Transactions;
+
+namespace TransactScope_IEnlistmentNotification_2PC
+{
+    public class FileResourceManager : IEnlistmentNotification
+    {
+        private string _tempFilePath;
+        private bool _hasCommited = false;
+        public FileResourceManager(string tempFilePath)
+        {
+            this._tempFilePath = tempFilePath;
+            if (Transaction.Current != null)
+            {
+                Transaction.Current.EnlistVolatile(this, EnlistmentOptions.None);
+            }            
+        }
+
+        public void Commit(Enlistment enlistment)
+        {
+            using (FileStream fs = File.Create(this._tempFilePath))
+            {
+                var data = new UTF8Encoding(true).GetBytes("Test text");
+                fs.Write(data, 0, data.Length);
+                _hasCommited = true;
+                enlistment.Done();
+            }
+        }
+
+        public void InDoubt(Enlistment enlistment)
+        {
+            enlistment.Done();
+        }
+
+        public void Prepare(PreparingEnlistment preparingEnlistment)
+        {
+            preparingEnlistment.Prepared();
+        }
+
+        public void Rollback(Enlistment enlistment)
+        {
+            try
+            {
+                if (this._hasCommited == false)
+                {
+                    File.Delete(this._tempFilePath);
+                }
+            }
+            finally
+            {
+                enlistment.Done();
+            }
+
+        }
+    }
+}
---
+++ TransactScope_IEnlistmentNotification_2PC/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Data.SqlClient;
+using System.Transactions;
+using TransactScope_IEnlistmentNotification_2PC;
+
+
+using (var transaction = new TransactionScope())
+{
+	//transaction sample 
+	//using (var context = new UserContext())
+	//{
+	//	context.Users.Add(new User() { });
+	//	context.SaveChanges();
+	//}
+
+    //You can use FileSystem operations, MessageQueue and other data source operations in TransactionScope. 
+    //this is not limited only database operations. 
+
+	var fileCreator = new FileResourceManager(Path.Combine(Path.GetTempPath(), "test.txt"));
+
+    using (var conn = new SqlConnection("YourConnectionString"))
+    {
+        using (SqlCommand cmd = conn.CreateCommand())
+        {
+            cmd.CommandText = "INSERT INTO User(Name) values ('Hakan');";
+            cmd.Connection.Open();
+            cmd.ExecuteNonQuery();
+        }
+    }
+
+    transaction.Complete();
+	
+
+
+}
